2dbfd273802c76b0f1c544d05ef6423391cb9780
Bomiro <105790086+Bomiro@users.noreply.github.com>
2023-02-19 15:59:55 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,10 +10,10 @@ module.exports = {
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'default' : process.env.ALIVE_MSG,
 LANG: process.env.LANG === undefined ? 'EN' : process.env.LANG,
 ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.imgur.com/v8iuP8v.jpg' : process.env.ALIVE_LOGO,
-FOOTER: process.env.FOOTER === undefined ? 'ᴀꜱᴛʀᴏ-ᴍᴅ ᴘᴜʙʟɪᴄ ꜱᴛᴀʙʟᴇ' : process.env.FOOTER,
-CAPTION: process.env.CAPTION === undefined ? '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀꜱᴛʀᴏ-ᴍᴅ*' : process.env.CAPTION,
-ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
-INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
+FOOTER: process.env.FOOTER === undefined ? 'BOMIBOT' : process.env.FOOTER,
+CAPTION: process.env.CAPTION === undefined ? '*BOMIBOT*' : process.env.CAPTION,
+ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'true' : process.env.ONLY_GROUP,
+INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*only group*' : process.env.INBOX_BL_MSG,
 HOST: process.env.HOST === undefined ? '-' : process.env.HOST,
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID
+SESSION_ID: process.env.SESSION_ID === undefined ? '3fab194e-242c-427a-968d-01eb91d2ac67' : process.env.SESSION_ID
 };
